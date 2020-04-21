4c4adbb219c34e0297bcd50c36df89fdb4c8655f
Luisa Martinez <luisamartinez@luisas-mbp.ibm.pok.com>
2020-04-21 17:55:14 -0400

    init commit

---
+++ Hello.py
@@ -0,0 +1,12 @@
+# This is to test GitHub 
+#this is a commmand 
+print('Hello, World!')
+print('What is your name?')
+myName = input()
+print('it is good to meet you, ' + myName)
+print('The length of your name is: ' )
+print(len(myName) )
+print('What is your age?')      #Ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge)+1) + ' in a year. ')
+
