98e163a2fb880b7918aaf1ffc7bd8d618c282a84
CHRISTOPHER FRITZ <cfritz@us.ibm.com>
2020-04-21 18:30:17 -0400

    cf first commit

---
+++ CFHelloWorld.py
@@ -0,0 +1,10 @@
+# comment
+
+print('hello'+'world')
+print('what is ur name')
+myName = input()
+print('nice to meet u '+ myName)
+print(len(myName))
+print('what is your age')
+myAge = input()
+print('you wille be ' + str(int(myAge)+1) + 'in a yr')
