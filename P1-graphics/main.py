30498ab8f43558d527b29da707b2da78541154c9
CHRISTOPHER FRITZ <cfritz@us.ibm.com>
2020-04-27 16:29:43 -0400

    added package

--- P1-graphics/main.py
+++ P1-graphics/main.py
@@ -1,1 +1,10 @@
 # Render some graphics
+# get python version: python -V (or python --version)
+# install package: pip3 install --user http://bit.ly/csc161graphics
+
+from graphics import *
+
+def main():
+    print('hello')
+
+main()
