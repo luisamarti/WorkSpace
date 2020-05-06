a486d36db58712b233fb759cc411acd87208217c
CHRISTOPHER FRITZ <cfritz@us.ibm.com>
2020-05-06 17:04:09 -0400

    set text properties

--- P1-graphics/main.py
+++ P1-graphics/main.py
@@ -9,9 +9,12 @@ from graphics import *
 
 def main():
     win = GraphWin("My Window", 500, 500)
-    #win.setBackground(color_rgb(0,0,0))
+    win.setBackground(color_rgb(0,0,0))
 
     txt = Text(Point(250,250), "what's up?")
+    txt.setTextColor(color_rgb(0,125,255))
+    txt.setSize(30)
+    txt.setFace('courier')
     txt.draw(win)
 
     win.getMouse()
