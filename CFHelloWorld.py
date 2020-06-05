ba4a3a085ffb1368eef228de14dda6b6e1d07b6d
CHRISTOPHER FRITZ <cfritz@us.ibm.com>
2020-06-05 09:47:17 -0400

    testing pass by value-pass by reference

--- CFHelloWorld.py
+++ CFHelloWorld.py
@@ -1,10 +1,32 @@
 # comment
 
-print('hello'+'world')
-print('what is ur name')
-myName = input()
-print('nice to meet u '+ myName)
-print(len(myName))
-print('what is your age')
-myAge = input()
-print('you wille be ' + str(int(myAge)+1) + 'in a yr')
+# print('hello'+'world')
+# print('what is ur name')
+# myName = input()
+# print('nice to meet u '+ myName)
+# print(len(myName))
+# print('what is your age')
+# myAge = input()
+# print('you wille be ' + str(int(myAge)+1) + 'in a yr')
+
+def myfunc(funcstr):
+  print("in function:"+funcstr)
+  funcstr = "junk"
+  print("in function 2:"+funcstr)
+  return funcstr
+
+# so far everything passes by value
+def myfunc2(funcarr):
+  print("in function:")
+  for item in funcarr:
+    print(item)
+  funcarr = ["junk","junk2"]
+  print("in function 2:")
+  for item in funcarr: 
+        print(item, end =' hi luisa lol ')
+  return
+
+myVar = ["string"]
+print(myVar)
+myfunc2(myVar)
+print(myVar)
