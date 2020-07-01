e69e327a9c7b941ec7841f22058e43bb725c8360
Luisa Martinez <luisamartinez@Luisas-MacBook-Pro.local>
2020-07-01 18:04:07 -0400

    Print weather

--- P3-APIs/main.py
+++ P3-APIs/main.py
@@ -11,8 +11,15 @@ resp = requests.get("http://api.openweathermap.org/data/2.5/weather?zip=94040,us
 if resp.status_code != 200:
     raise APIError(resp.status_code)
 
+#save json response into data 
 data = resp.json() 
-print(data)
-
+#print(data)
 # try parsing data - how do you view or extract a prop
-print(json.dumps(json.loads(data).weather))
+print(json.dumps(data, indent=4))
+weatherDesc =  "Description " + data["weather"][0]["description"]
+weatherTemp =  "Temperature "  + str(data["main"]["temp"])
+tempK = data["main"]["temp"]
+tempF =  round(((9/5) * tempK) - 459.67,2)
+print(weatherDesc)
+print(tempK)
+print( "weather Temp F " + str(tempF))
