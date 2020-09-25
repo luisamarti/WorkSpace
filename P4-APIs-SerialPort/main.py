1c18e77a78e37bae81593508ac9a4f9856bad65e
CHRISTOPHER FRITZ <cfritz@us.ibm.com>
2020-09-25 18:37:01 -0400

    misc

--- P4-APIs-SerialPort/main.py
+++ P4-APIs-SerialPort/main.py
@@ -32,7 +32,6 @@ def main():
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
         print(message)
-        
 
     print( myPort )
 
@@ -41,14 +40,9 @@ def main():
     print(zipcode)
     print(type(zipcode))
 
-
-    # once we have the port to use and zipcode, we will do an API request every x minutes and then send the weather data obtained from the API to the selected
-    # port. This will check if is new data
-
-
-    # api-endpoint 
-    # insert user input into api call https://matthew-brett.github.io/teaching/string_formatting.html
-    #URL = "api.openweathermap.org/data/2.5/weather"
+    # once we have the port to use and zipcode, 
+    # we will do an API request every x minutes and then 
+    # send the weather data obtained from the API to the selected port.
     i=0
     while True: 
         # Indicate what iteration we're on
@@ -56,13 +50,24 @@ def main():
         print(i)
 
         # Get the weather data via the API
+        # insert user input into api call https://matthew-brett.github.io/teaching/string_formatting.html
+        # URL = "api.openweathermap.org/data/2.5/weather"
         resp = requests.get("http://api.openweathermap.org/data/2.5/weather?zip={},us&appid=e28b09913aed610ae9e48d01fbb747a8".format(zipcode))
         if resp.status_code != 200:
             raise APIError(resp.status_code)
-        #save json response into data 
+        # save json response into data 
         data = resp.json() 
+
         # Print entire API response for debug
         #print(json.dumps(data, indent=4))
+
+        # Get temperature data from the response
+        tempK = data["main"]["temp"]
+        tempF = round(((9/5) * tempK) - 459.67,2)
+        # (1) Round to nearest integer (nearest even int for "tie"),
+        # (2) convert to int (remove ".0"),
+        # (3) convert to string for transmission over serial
+        tempFstr = str(int(round(tempF,0)))
 
         # Print the time
         t = time.localtime()
@@ -73,16 +78,15 @@ def main():
         weatherDesc =  "Description " + data["weather"][0]["description"]
         weatherTemp =  "Temperature "  + str(data["main"]["temp"])
         weatherIconNum = data["weather"][0]["icon"]
-        tempK = data["main"]["temp"]
-        tempF =  round(((9/5) * tempK) - 459.67,2)
-        print(weatherIconNum)
         print(weatherDesc)
-        print(tempK)
-        print( "weather Temp F " + str(tempF))
+        print(weatherIconNum)
+        print( "Temperature in K:" + str(tempK))
+        print( "Temperature in F:" + str(tempF))
 
         # Send the icon code to the Arduino
         #==> use these codes: https://openweathermap.org/weather-conditions
         myPort.write(weatherIconNum.encode())
+        myPort.write(tempFstr.encode())
 
         # Test read the serial port, using a wire to loopback TX into RX on the port.
         # clear from prev iterations
@@ -92,12 +96,10 @@ def main():
         readvar = myPort.read_all()
         print(readvar)
         
-        #for testing purposes, we will send the request every 5 seconds
+        # Wait until next request
+        # for testing purposes, we will send the request every 5 seconds
         time.sleep(5)
 
-
-def convertDescriptionToNumber(descriptionString):
-    print('convert function')
 
 
 main()
